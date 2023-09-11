9dd0aae0bec75bb1835856866bd5758fa2664b1c
w3App <kshatriyakabawat@gmail.com>
2023-09-11 09:32:49 +0530

    image live code

--- src/components/Result.js
+++ src/components/Result.js
@@ -9,26 +9,21 @@ const Result = ({ result, setResult }) => {
     const unique_id = uuid();
     const small_id = unique_id.slice(0, 5)
     const router = useRouter()
+
     // const handleRedirect = (code) => {
 
     //     setTimeout(() => {
     //         router.push(`result/techkilla-${small_id}_m`);
     //     }, 1000)
     // }
+
     const handleRedirect = () => {
-        // Create a Blob object from the base64 image data
-        const base64Image = result; // Assuming 'code' contains the base64 image data
-        const blob = new Blob([base64Image], { type: 'image/jpeg' }); // Adjust the type as needed (e.g., 'image/png' for PNG images)
-
-        // Create a temporary URL for the Blob
+        const base64Image = result;
+        const blob = new Blob([base64Image], { type: 'image/jpeg' });
         const url = window.URL.createObjectURL(blob);
-
-        // Create an anchor element to trigger the download
         const a = document.createElement('a');
         a.href = url;
-        a.download = `tk-${small_id}_m.webp`; // Specify the desired file name
-
-        // Programmatically trigger a click event on the anchor element
+        a.download = `${small_id}.webp`;
         const clickEvent = new MouseEvent('click', {
             view: window,
             bubbles: true,
@@ -40,7 +35,7 @@ const Result = ({ result, setResult }) => {
         window.URL.revokeObjectURL(url);
 
         setTimeout(() => {
-            router.push(`result/tk-${small_id}_m`);
+            router.push(`result/${small_id}`);
         }, 1000)
     };
 
