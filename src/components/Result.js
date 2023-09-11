152964de29a17ce210e58e30e1c9ab5b77662873
w3App <kshatriyakabawat@gmail.com>
2023-09-11 09:28:33 +0530

    live image

--- src/components/Result.js
+++ src/components/Result.js
@@ -9,12 +9,41 @@ const Result = ({ result, setResult }) => {
     const unique_id = uuid();
     const small_id = unique_id.slice(0, 8)
     const router = useRouter()
-    const handleRedirect = (code) => {
+    // const handleRedirect = (code) => {
+
+    //     setTimeout(() => {
+    //         router.push(`result/techkilla-${small_id}_m`);
+    //     }, 1000)
+    // }
+    const handleRedirect = () => {
+        // Create a Blob object from the base64 image data
+        const base64Image = result; // Assuming 'code' contains the base64 image data
+        const blob = new Blob([base64Image], { type: 'image/jpeg' }); // Adjust the type as needed (e.g., 'image/png' for PNG images)
+
+        // Create a temporary URL for the Blob
+        const url = window.URL.createObjectURL(blob);
+
+        // Create an anchor element to trigger the download
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = `tk-${small_id}_m.webp`; // Specify the desired file name
+
+        // Programmatically trigger a click event on the anchor element
+        const clickEvent = new MouseEvent('click', {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
+        a.dispatchEvent(clickEvent);
+
+        // Cleanup by revoking the Blob URL
+        window.URL.revokeObjectURL(url);
 
         setTimeout(() => {
-            router.push(`result/techkilla-${small_id}_m`);
+            router.push(`result/tk-${small_id}_m`);
         }, 1000)
-    }
+    };
+
     return (
         <div className='center_main py-5 '>
             <h1 className='text-center'>Here is Your Photograph</h1>
@@ -29,7 +58,8 @@ const Result = ({ result, setResult }) => {
                                 <button className='btn btn-warning wt-border start-btn' onClick={() => setResult('')}>Re-generate</button>
                             </div>
                             <div>
-                                <a href={result} onClick={handleRedirect} target="_blank" rel="noopener noreferrer" download={`techkilla-${small_id}_m`} className='btn wt-border btn-warning start-btn'>Save</a>
+                                <button onClick={handleRedirect} className='btn wt-border btn-warning start-btn'>download</button>
+                                {/* <a href={result} onClick={handleRedirect} target="_blank" rel="noopener noreferrer" download={`tk-${small_id}_m`} className='btn wt-border btn-warning start-btn'>Save</a> */}
                             </div>
                         </div>
                     </Col>
